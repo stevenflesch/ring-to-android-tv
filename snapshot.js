f7d93aab8cb089bca37c20b13e896dcff6f6e0d6
Steven Flesch <steven.flesch@gmail.com>
2020-03-28 15:02:11 -0500

    Update snapshot.js
    - Add header description
    - Move apiToken to getApiToken() method
    - Add doc to sendNotification() method
    - Send blank string to PiPup if no imageFile specified in sendNotification() argument
    - Print error and exit script on failure to send notification
    - Print snapshot image size instead of arraybuffer object
    - Move ringApi object setup to bottom of script.

--- snapshot.js
+++ snapshot.js
@@ -1,6 +1,9 @@
-/**
+/*
  * snapshot.js
- * This file is used for debugging purposes, it simply retrieves a snapshot and sends it to PiPup.
+ * This file is used for debugging purposes, it simply retrieves a snapshot and sends it to PiPup
+ * for display on an Android TV device.
+ * 
+ * Remember to change the tvIpAddress variable and save your API token to token.txt.
  */
 
 // Dependencies
@@ -9,19 +12,29 @@ const fs = require('fs')
 const request = require('request')
 
 // Configuration
-const apiToken = fs.readFileSync('token.txt').toString().trim()     // Grab plain-text token from token.txt
 const tvIpAddress = '192.168.1.11'                                  // IP address of the Android TV you are running PiPup on
 const displayTime = 20                                              // Display time for notifications, in seconds
 const opacity = 100                                                 // Opacity of notification background, in percentage
 
-// Set up Ring API object
-ringApi = new Ring.RingApi({
-    refreshToken: apiToken,
-    cameraDingsPollingSeconds: 2
-})
+/**
+ * Returns the API token from `token.txt` or gracefully exits the script with error.
+ */
+function getApiToken() {
+    try {
+        return(fs.readFileSync('token.txt').toString().trim())     // Grab plain-text token from token.txt
+    } catch(err) {
+        console.log('Unable to read API token from token.txt - ensure you have an API token before running the script.')
+        process.exit(-1)
+    }
+}
 
-// Send notification to PiPup
-async function sendNotification(title, message, imageFile) {
+/**
+ * Sends a notification to PiPup app on Android TV.
+ * @param {*} title Title of notification message.
+ * @param {*} message Text of notification message.
+ * @param {*} imageFile Path to image file, can be blank string to display no image.
+ */
+async function sendNotification(title, message, imageFile) {    
     const options = {
         method: "POST",
         url: "http://" + tvIpAddress + ":7979/notify",
@@ -29,7 +42,7 @@ async function sendNotification(title, message, imageFile) {
         headers: {
             "Content-Type": "multipart/form-data"
         },
-        formData : {
+        formData: {
             "duration": displayTime,
             "position": 0,
             "title": title,
@@ -39,7 +52,7 @@ async function sendNotification(title, message, imageFile) {
             "messageColor": "#000000",
             "messageSize": 14,
             "backgroundColor": "#ffffff",
-            "image" : fs.createReadStream(imageFile),
+            "image" : (imageFile == '') ? "" : fs.createReadStream(imageFile),
             "imageWidth": 640
         }
     }
@@ -47,7 +60,9 @@ async function sendNotification(title, message, imageFile) {
     // Fire off POST message to PiPup with 'request'
     request(options, function (err, res, body) {
         if(err) {
+            console.log('Error sending notification.')
             console.log(err)
+            process.exit(-1)
         } else {
             console.log('Sent notification successfully.')
             process.exit()  // exit the script for debugging!
@@ -62,7 +77,7 @@ async function getSnapshot() {
     
     try {
         const snapshotBuffer = await camera.getSnapshot()
-        console.log(snapshotBuffer)
+        console.log('Snapshot size: ' + Math.floor(snapshotBuffer.byteLength/1024) + ' kb')
     
         fs.writeFile('snapshot.png', snapshotBuffer, (err) => {
             // throws an error, you could also catch it here
@@ -79,4 +94,12 @@ async function getSnapshot() {
     }
 }
 
+// Set up Ring API object
+ringApi = new Ring.RingApi({
+    refreshToken: getApiToken(),
+    controlCenterDisplayName: 'ring-to-android-tv',
+    cameraDingsPollingSeconds: 2
+})
+
+// Grab snapshot and exit!
 getSnapshot()
